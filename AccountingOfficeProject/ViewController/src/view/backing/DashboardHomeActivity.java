6d77fa23e002ef3dc27427e4269b773b61f654c4
AhmedMAghani <Ahmed.m.3bd.elghani@gmail.com>
2020-03-25 16:45:01 +0200

    Add Chart

--- AccountingOfficeProject/ViewController/src/view/backing/DashboardHomeActivity.java
+++ AccountingOfficeProject/ViewController/src/view/backing/DashboardHomeActivity.java
@@ -1,7 +1,32 @@
 package view.backing;
 
+import java.text.DateFormat;
+
+import java.text.SimpleDateFormat;
+
+import javax.faces.context.FacesContext;
+
+import model.BCs.VOs.Contracts_VOImpl;
+
+import model.BCs.VOs.Contracts_VORowImpl;
+
+import model.BCs.VOs.Tasks_VOImpl;
+
+import oracle.adf.model.AttributeBinding;
+import oracle.adf.model.BindingContainer;
+import oracle.adf.model.BindingContext;
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.adf.model.generic.DCRowSetIteratorImpl;
+import oracle.adf.view.faces.bi.component.chart.UIBarChart;
+import oracle.adf.view.faces.bi.component.chart.UIDataItem;
+import oracle.adf.view.faces.bi.component.chart.UILegend;
+import oracle.adf.view.faces.bi.component.gauge.UIMetricLabel;
+import oracle.adf.view.faces.bi.component.gauge.UIStatusMeterGauge;
+import oracle.adf.view.faces.bi.component.gauge.UIThreshold;
 import oracle.adf.view.rich.component.rich.RichDocument;
 import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.data.RichListItem;
+import oracle.adf.view.rich.component.rich.data.RichListView;
 import oracle.adf.view.rich.component.rich.fragment.RichPageTemplate;
 import oracle.adf.view.rich.component.rich.layout.RichGridCell;
 import oracle.adf.view.rich.component.rich.layout.RichGridRow;
@@ -9,6 +34,8 @@ import oracle.adf.view.rich.component.rich.layout.RichPanelBox;
 import oracle.adf.view.rich.component.rich.layout.RichPanelGridLayout;
 import oracle.adf.view.rich.component.rich.nav.RichButton;
 import oracle.adf.view.rich.component.rich.output.RichMessages;
+import oracle.adf.view.rich.component.rich.output.RichOutputFormatted;
+
 
 public class DashboardHomeActivity {
     private RichPageTemplate pt1;
@@ -43,6 +70,52 @@ public class DashboardHomeActivity {
     private RichPanelBox pb7;
     private RichPanelBox pb8;
     private RichPanelBox pb9;
+    private RichListView lv1;
+    private RichListItem li1;
+    private RichPanelGridLayout pgl2;
+    private RichGridRow gr6;
+    private RichGridCell gc10;
+    private RichOutputFormatted of1;
+    private RichGridCell gc11;
+    private RichGridCell gc12;
+    private RichOutputFormatted of3;
+    private RichGridCell gc13;
+    private RichGridCell gc14;
+    private RichGridCell gc15;
+    private UIStatusMeterGauge statusMeterGauge1;
+    private UIMetricLabel gml1;
+    private UIThreshold thr1;
+    private UIThreshold thr2;
+    private UIThreshold thr3;
+    private UIStatusMeterGauge statusMeterGauge2;
+    private UIMetricLabel gml2;
+    private UIThreshold thr4;
+    private UIThreshold thr5;
+    private UIThreshold thr6;
+    private UIStatusMeterGauge statusMeterGauge3;
+    private UIMetricLabel gml3;
+    private UIThreshold thr7;
+    private UIThreshold thr8;
+    private UIStatusMeterGauge statusMeterGauge4;
+    private UIMetricLabel gml4;
+    private UIThreshold thr9;
+    private UIThreshold thr10;
+    private RichGridRow gr7;
+    private RichGridCell gc16;
+    private RichGridCell gc17;
+    private RichGridCell gc18;
+    private RichGridCell gc19;
+    private RichGridCell gc20;
+    private RichGridCell gc21;
+    private UIBarChart barChart1;
+    private UILegend cl1;
+    private UIDataItem di1;
+    private Integer totalContractNo;
+    private Integer completedContractNo;
+    private Integer issuedContractNo;
+    private Integer totalTasksNo;
+    private Integer completedTasksNo;
+    private Integer issuedTasksNo;
 
     public void setPt1(RichPageTemplate pt1) {
         this.pt1 = pt1;
@@ -300,5 +373,349 @@ public class DashboardHomeActivity {
 
     public RichPanelBox getPb9() {
         return pb9;
+    }
+
+
+    public void setLv1(RichListView lv1) {
+        this.lv1 = lv1;
+    }
+
+    public RichListView getLv1() {
+        return lv1;
+    }
+
+    public void setLi1(RichListItem li1) {
+        this.li1 = li1;
+    }
+
+    public RichListItem getLi1() {
+        return li1;
+    }
+
+    public void setPgl2(RichPanelGridLayout pgl2) {
+        this.pgl2 = pgl2;
+    }
+
+    public RichPanelGridLayout getPgl2() {
+        return pgl2;
+    }
+
+    public void setGr6(RichGridRow gr6) {
+        this.gr6 = gr6;
+    }
+
+    public RichGridRow getGr6() {
+        return gr6;
+    }
+
+    public void setGc10(RichGridCell gc10) {
+        this.gc10 = gc10;
+    }
+
+    public RichGridCell getGc10() {
+        return gc10;
+    }
+
+    public void setOf1(RichOutputFormatted of1) {
+        this.of1 = of1;
+    }
+
+    public RichOutputFormatted getOf1() {
+        return of1;
+    }
+
+    public void setGc11(RichGridCell gc11) {
+        this.gc11 = gc11;
+    }
+
+    public RichGridCell getGc11() {
+        return gc11;
+    }
+
+
+    public void setGc12(RichGridCell gc12) {
+        this.gc12 = gc12;
+    }
+
+    public RichGridCell getGc12() {
+        return gc12;
+    }
+
+    public void setOf3(RichOutputFormatted of3) {
+        this.of3 = of3;
+    }
+
+    public RichOutputFormatted getOf3() {
+        return of3;
+    }
+
+    public void setGc13(RichGridCell gc13) {
+        this.gc13 = gc13;
+    }
+
+    public RichGridCell getGc13() {
+        return gc13;
+    }
+
+
+    public void setGc14(RichGridCell gc14) {
+        this.gc14 = gc14;
+    }
+
+    public RichGridCell getGc14() {
+        return gc14;
+    }
+
+
+    public void setGc15(RichGridCell gc15) {
+        this.gc15 = gc15;
+    }
+
+    public RichGridCell getGc15() {
+        return gc15;
+    }
+
+
+    public void setStatusMeterGauge1(UIStatusMeterGauge statusMeterGauge1) {
+        this.statusMeterGauge1 = statusMeterGauge1;
+    }
+
+    public UIStatusMeterGauge getStatusMeterGauge1() {
+        return statusMeterGauge1;
+    }
+
+    public void setGml1(UIMetricLabel gml1) {
+        this.gml1 = gml1;
+    }
+
+    public UIMetricLabel getGml1() {
+        return gml1;
+    }
+
+    public void setThr1(UIThreshold thr1) {
+        this.thr1 = thr1;
+    }
+
+    public UIThreshold getThr1() {
+        return thr1;
+    }
+
+    public void setThr2(UIThreshold thr2) {
+        this.thr2 = thr2;
+    }
+
+    public UIThreshold getThr2() {
+        return thr2;
+    }
+
+    public void setThr3(UIThreshold thr3) {
+        this.thr3 = thr3;
+    }
+
+    public UIThreshold getThr3() {
+        return thr3;
+    }
+
+    public void setStatusMeterGauge2(UIStatusMeterGauge statusMeterGauge2) {
+        this.statusMeterGauge2 = statusMeterGauge2;
+    }
+
+    public UIStatusMeterGauge getStatusMeterGauge2() {
+        return statusMeterGauge2;
+    }
+
+    public void setGml2(UIMetricLabel gml2) {
+        this.gml2 = gml2;
+    }
+
+    public UIMetricLabel getGml2() {
+        return gml2;
+    }
+
+    public void setThr4(UIThreshold thr4) {
+        this.thr4 = thr4;
+    }
+
+    public UIThreshold getThr4() {
+        return thr4;
+    }
+
+    public void setThr5(UIThreshold thr5) {
+        this.thr5 = thr5;
+    }
+
+    public UIThreshold getThr5() {
+        return thr5;
+    }
+
+    public void setThr6(UIThreshold thr6) {
+        this.thr6 = thr6;
+    }
+
+    public UIThreshold getThr6() {
+        return thr6;
+    }
+
+    public void setStatusMeterGauge3(UIStatusMeterGauge statusMeterGauge3) {
+        this.statusMeterGauge3 = statusMeterGauge3;
+    }
+
+    public UIStatusMeterGauge getStatusMeterGauge3() {
+        return statusMeterGauge3;
+    }
+
+    public void setGml3(UIMetricLabel gml3) {
+        this.gml3 = gml3;
+    }
+
+    public UIMetricLabel getGml3() {
+        return gml3;
+    }
+
+    public void setThr7(UIThreshold thr7) {
+        this.thr7 = thr7;
+    }
+
+    public UIThreshold getThr7() {
+        return thr7;
+    }
+
+    public void setThr8(UIThreshold thr8) {
+        this.thr8 = thr8;
+    }
+
+    public UIThreshold getThr8() {
+        return thr8;
+    }
+
+    public void setStatusMeterGauge4(UIStatusMeterGauge statusMeterGauge4) {
+        this.statusMeterGauge4 = statusMeterGauge4;
+    }
+
+    public UIStatusMeterGauge getStatusMeterGauge4() {
+        return statusMeterGauge4;
+    }
+
+    public void setGml4(UIMetricLabel gml4) {
+        this.gml4 = gml4;
+    }
+
+    public UIMetricLabel getGml4() {
+        return gml4;
+    }
+
+    public void setThr9(UIThreshold thr9) {
+        this.thr9 = thr9;
+    }
+
+    public UIThreshold getThr9() {
+        return thr9;
+    }
+
+    public void setThr10(UIThreshold thr10) {
+        this.thr10 = thr10;
+    }
+
+    public UIThreshold getThr10() {
+        return thr10;
+    }
+
+    public void setGr7(RichGridRow gr7) {
+        this.gr7 = gr7;
+    }
+
+    public RichGridRow getGr7() {
+        return gr7;
+    }
+
+    public void setGc16(RichGridCell gc16) {
+        this.gc16 = gc16;
+    }
+
+    public RichGridCell getGc16() {
+        return gc16;
+    }
+
+    public void setGc17(RichGridCell gc17) {
+        this.gc17 = gc17;
+    }
+
+    public RichGridCell getGc17() {
+        return gc17;
+    }
+
+    public void setGc18(RichGridCell gc18) {
+        this.gc18 = gc18;
+    }
+
+    public RichGridCell getGc18() {
+        return gc18;
+    }
+
+    public void setGc19(RichGridCell gc19) {
+        this.gc19 = gc19;
+    }
+
+    public RichGridCell getGc19() {
+        return gc19;
+    }
+
+    public void setGc20(RichGridCell gc20) {
+        this.gc20 = gc20;
+    }
+
+    public RichGridCell getGc20() {
+        return gc20;
+    }
+
+    public void setGc21(RichGridCell gc21) {
+        this.gc21 = gc21;
+    }
+
+    public RichGridCell getGc21() {
+        return gc21;
+    }
+
+
+    public void setBarChart1(UIBarChart barChart1) {
+        this.barChart1 = barChart1;
+    }
+
+    public UIBarChart getBarChart1() {
+        return barChart1;
+    }
+
+    public void setCl1(UILegend cl1) {
+        this.cl1 = cl1;
+    }
+
+    public UILegend getCl1() {
+        return cl1;
+    }
+
+    public void setDi1(UIDataItem di1) {
+        this.di1 = di1;
+    }
+
+    public UIDataItem getDi1() {
+        return di1;
+    }
+    
+    private void setContractInfo(){
+        BindingContext bc = BindingContext.getCurrent();
+        DCBindingContainer dcbc = (DCBindingContainer)bc.getCurrentBindingsEntry();
+        Contracts_VOImpl contractVO =(Contracts_VOImpl) dcbc.findIteratorBinding("Contracts1Iterator").getViewObject();
+        totalContractNo =contractVO.getTotalContractsNo();
+        completedContractNo = contractVO.getCompletedContractsNo();
+        issuedContractNo = contractVO.getIssuedContractsNo();
+    }
+    
+    private void setTasksInfo(){
+        BindingContext bc = BindingContext.getCurrent();
+        DCBindingContainer dcbc = (DCBindingContainer)bc.getCurrentBindingsEntry();
+        Tasks_VOImpl contractVO =(Tasks_VOImpl) dcbc.findIteratorBinding("Tasks1Iterator").getViewObject();
+        totalTasksNo = contractVO.getTotalTasksNo();
+        completedTasksNo = contractVO.getCompletedTasksNo();
+        issuedTasksNo = contractVO.getIssuedTasksNo();
     }
 }
