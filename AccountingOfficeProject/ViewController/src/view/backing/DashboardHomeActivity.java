31cfd7f436eeff5212baadda565c32076b101502
AhmedMAghani <Ahmed.m.3bd.elghani@gmail.com>
2020-03-25 09:52:26 +0200

    Initialize project

---
+++ AccountingOfficeProject/ViewController/src/view/backing/DashboardHomeActivity.java
@@ -0,0 +1,304 @@
+package view.backing;
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.fragment.RichPageTemplate;
+import oracle.adf.view.rich.component.rich.layout.RichGridCell;
+import oracle.adf.view.rich.component.rich.layout.RichGridRow;
+import oracle.adf.view.rich.component.rich.layout.RichPanelBox;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGridLayout;
+import oracle.adf.view.rich.component.rich.nav.RichButton;
+import oracle.adf.view.rich.component.rich.output.RichMessages;
+
+public class DashboardHomeActivity {
+    private RichPageTemplate pt1;
+    private RichForm f1;
+    private RichDocument d1;
+    private RichButton b1;
+    private RichButton b2;
+    private RichButton b3;
+    private RichButton b4;
+    private RichMessages m1;
+    private RichPanelGridLayout pgl1;
+    private RichGridRow gr1;
+    private RichGridCell gc1;
+    private RichGridRow gr2;
+    private RichGridCell gc2;
+    private RichGridCell gc3;
+    private RichGridCell gc4;
+    private RichGridRow gr3;
+    private RichGridCell gc5;
+    private RichGridRow gr4;
+    private RichGridCell gc6;
+    private RichGridCell gc7;
+    private RichGridCell gc8;
+    private RichGridRow gr5;
+    private RichGridCell gc9;
+    private RichPanelBox pb1;
+    private RichPanelBox pb2;
+    private RichPanelBox pb3;
+    private RichPanelBox pb4;
+    private RichPanelBox pb5;
+    private RichPanelBox pb6;
+    private RichPanelBox pb7;
+    private RichPanelBox pb8;
+    private RichPanelBox pb9;
+
+    public void setPt1(RichPageTemplate pt1) {
+        this.pt1 = pt1;
+    }
+
+    public RichPageTemplate getPt1() {
+        return pt1;
+    }
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+
+    public void setB1(RichButton b1) {
+        this.b1 = b1;
+    }
+
+    public RichButton getB1() {
+        return b1;
+    }
+
+    public void setB2(RichButton b2) {
+        this.b2 = b2;
+    }
+
+    public RichButton getB2() {
+        return b2;
+    }
+
+    public void setB3(RichButton b3) {
+        this.b3 = b3;
+    }
+
+    public RichButton getB3() {
+        return b3;
+    }
+
+    public void setB4(RichButton b4) {
+        this.b4 = b4;
+    }
+
+    public RichButton getB4() {
+        return b4;
+    }
+
+
+    public void setM1(RichMessages m1) {
+        this.m1 = m1;
+    }
+
+    public RichMessages getM1() {
+        return m1;
+    }
+
+
+    public void setPgl1(RichPanelGridLayout pgl1) {
+        this.pgl1 = pgl1;
+    }
+
+    public RichPanelGridLayout getPgl1() {
+        return pgl1;
+    }
+
+    public void setGr1(RichGridRow gr1) {
+        this.gr1 = gr1;
+    }
+
+    public RichGridRow getGr1() {
+        return gr1;
+    }
+
+    public void setGc1(RichGridCell gc1) {
+        this.gc1 = gc1;
+    }
+
+    public RichGridCell getGc1() {
+        return gc1;
+    }
+
+    public void setGr2(RichGridRow gr2) {
+        this.gr2 = gr2;
+    }
+
+    public RichGridRow getGr2() {
+        return gr2;
+    }
+
+    public void setGc2(RichGridCell gc2) {
+        this.gc2 = gc2;
+    }
+
+    public RichGridCell getGc2() {
+        return gc2;
+    }
+
+    public void setGc3(RichGridCell gc3) {
+        this.gc3 = gc3;
+    }
+
+    public RichGridCell getGc3() {
+        return gc3;
+    }
+
+    public void setGc4(RichGridCell gc4) {
+        this.gc4 = gc4;
+    }
+
+    public RichGridCell getGc4() {
+        return gc4;
+    }
+
+    public void setGr3(RichGridRow gr3) {
+        this.gr3 = gr3;
+    }
+
+    public RichGridRow getGr3() {
+        return gr3;
+    }
+
+    public void setGc5(RichGridCell gc5) {
+        this.gc5 = gc5;
+    }
+
+    public RichGridCell getGc5() {
+        return gc5;
+    }
+
+    public void setGr4(RichGridRow gr4) {
+        this.gr4 = gr4;
+    }
+
+    public RichGridRow getGr4() {
+        return gr4;
+    }
+
+    public void setGc6(RichGridCell gc6) {
+        this.gc6 = gc6;
+    }
+
+    public RichGridCell getGc6() {
+        return gc6;
+    }
+
+    public void setGc7(RichGridCell gc7) {
+        this.gc7 = gc7;
+    }
+
+    public RichGridCell getGc7() {
+        return gc7;
+    }
+
+    public void setGc8(RichGridCell gc8) {
+        this.gc8 = gc8;
+    }
+
+    public RichGridCell getGc8() {
+        return gc8;
+    }
+
+    public void setGr5(RichGridRow gr5) {
+        this.gr5 = gr5;
+    }
+
+    public RichGridRow getGr5() {
+        return gr5;
+    }
+
+    public void setGc9(RichGridCell gc9) {
+        this.gc9 = gc9;
+    }
+
+    public RichGridCell getGc9() {
+        return gc9;
+    }
+
+    public void setPb1(RichPanelBox pb1) {
+        this.pb1 = pb1;
+    }
+
+    public RichPanelBox getPb1() {
+        return pb1;
+    }
+
+    public void setPb2(RichPanelBox pb2) {
+        this.pb2 = pb2;
+    }
+
+    public RichPanelBox getPb2() {
+        return pb2;
+    }
+
+    public void setPb3(RichPanelBox pb3) {
+        this.pb3 = pb3;
+    }
+
+    public RichPanelBox getPb3() {
+        return pb3;
+    }
+
+    public void setPb4(RichPanelBox pb4) {
+        this.pb4 = pb4;
+    }
+
+    public RichPanelBox getPb4() {
+        return pb4;
+    }
+
+    public void setPb5(RichPanelBox pb5) {
+        this.pb5 = pb5;
+    }
+
+    public RichPanelBox getPb5() {
+        return pb5;
+    }
+
+    public void setPb6(RichPanelBox pb6) {
+        this.pb6 = pb6;
+    }
+
+    public RichPanelBox getPb6() {
+        return pb6;
+    }
+
+    public void setPb7(RichPanelBox pb7) {
+        this.pb7 = pb7;
+    }
+
+    public RichPanelBox getPb7() {
+        return pb7;
+    }
+
+    public void setPb8(RichPanelBox pb8) {
+        this.pb8 = pb8;
+    }
+
+    public RichPanelBox getPb8() {
+        return pb8;
+    }
+
+    public void setPb9(RichPanelBox pb9) {
+        this.pb9 = pb9;
+    }
+
+    public RichPanelBox getPb9() {
+        return pb9;
+    }
+}
---
+++ AccountingOfficeProject/ViewController/src/view/backing/ManagmentHomeActivity.java
@@ -0,0 +1,72 @@
+package view.backing;
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.fragment.RichPageTemplate;
+import oracle.adf.view.rich.component.rich.nav.RichButton;
+
+public class ManagmentHomeActivity {
+    private RichPageTemplate pt1;
+    private RichForm f1;
+    private RichDocument d1;
+    private RichButton b1;
+    private RichButton b2;
+    private RichButton b3;
+    private RichButton b4;
+
+    public void setPt1(RichPageTemplate pt1) {
+        this.pt1 = pt1;
+    }
+
+    public RichPageTemplate getPt1() {
+        return pt1;
+    }
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+
+    public void setB1(RichButton b1) {
+        this.b1 = b1;
+    }
+
+    public RichButton getB1() {
+        return b1;
+    }
+
+    public void setB2(RichButton b2) {
+        this.b2 = b2;
+    }
+
+    public RichButton getB2() {
+        return b2;
+    }
+
+    public void setB3(RichButton b3) {
+        this.b3 = b3;
+    }
+
+    public RichButton getB3() {
+        return b3;
+    }
+
+    public void setB4(RichButton b4) {
+        this.b4 = b4;
+    }
+
+    public RichButton getB4() {
+        return b4;
+    }
+}
